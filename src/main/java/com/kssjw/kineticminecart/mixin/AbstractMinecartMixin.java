f8e9e85ed3e160eb4d03c1211a97a27adddf00b7
KSSJW <ZYJ2352401938@163.com>
2025-11-11 11:15:02 +0800

    Refactor: The project was refactored.

--- src/main/java/com/kssjw/kineticminecart/mixin/AbstractMinecartMixin.java
+++ src/main/java/com/kssjw/kineticminecart/mixin/AbstractMinecartMixin.java
@@ -1,12 +1,13 @@
 package com.kssjw.kineticminecart.mixin;
 
 import com.kssjw.kineticminecart.damage.MinecartImpactHandler;
+import com.kssjw.kineticminecart.knock.MinecartKonckHandler;
+import com.kssjw.kineticminecart.util.SpeedJudge;
 
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.vehicle.AbstractMinecart;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,24 +26,28 @@ public abstract class AbstractMinecartMixin {
         Level level = self.level(); 
         if (level == null || level.isClientSide()) return; // 仅服务端处理
 
-        // 速度短路：若矿车速度不足则跳过（避免频繁查询实体）
-        Vec3 mv = self.getDeltaMovement();
-        double speedSqr = mv.lengthSqr();
-        double threshold = MinecartImpactHandler.MIN_SPEED_THRESHOLD;
-        if (speedSqr < threshold * threshold) return;
+        SpeedJudge.catchedSpeed = SpeedJudge.getSpeed(self);
 
-        // 检测范围
-        double radius = 0.5;
-        AABB box = self.getBoundingBox().inflate(radius);
+        // Testing Logger
+        // Logger.printSpeed(SpeedJudge.catchedSpeed);
 
-        // 获取附近实体并过滤一些不需要的类型（自身、已死、不可交互等）
-        List<Entity> list = level.getEntities(self, box, e -> e != self && e.isAlive());
-        if (list.isEmpty()) return;
+        // 若矿车速度不足则跳过,避免频繁查询实体，最后触发生物上车
+        if (SpeedJudge.catchedSpeed > 2) {
 
-        for (Entity target : list) {
+            // 检测范围
+            double radius = 0.5;
+            AABB box = self.getBoundingBox().inflate(radius);
 
-            // 对每个目标调用 handler（handler 内部处理冷却/服务端校验）
-            MinecartImpactHandler.tryApplyImpact(self, target);
-        }
+            // 获取附近实体并过滤一些不需要的类型（自身、已死、不可交互等）
+            List<Entity> list = level.getEntities(self, box, e -> e != self && e.isAlive());
+            if (list.isEmpty()) return;
+
+            for (Entity target : list) {
+
+                // 对每个目标调用 handler（handler 内部处理冷却/服务端校验）
+                MinecartImpactHandler.tryApplyImpact(self, target);
+                MinecartKonckHandler.tryApplyKonck(self, target);
+            }
+        } // else MinecartAutoMount.maybeAutoMount(self);   // 备用，如果生物无法自动乘坐矿车则启用
     }
 }
