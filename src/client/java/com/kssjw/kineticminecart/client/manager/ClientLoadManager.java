a04a67086635f582b7254dbf01d5c632081e5c95
Pink Garrett <pink@thepinkhacker.com>
2026-04-16 23:25:59 -0400

    Simplify expressions in client code
    I forgot to check those classes.

--- src/client/java/com/kssjw/kineticminecart/client/manager/ClientLoadManager.java
+++ src/client/java/com/kssjw/kineticminecart/client/manager/ClientLoadManager.java
@@ -2,18 +2,10 @@ package com.kssjw.kineticminecart.client.manager;
 
 public class ClientLoadManager {
     public static boolean isClientAPIFound() {
-        boolean apiFound;
-
         try {
             Class.forName("me.shedaniel.autoconfig.AutoConfigClient");
-            apiFound = true;
-        } catch (ClassNotFoundException e) {
-            apiFound = false;
-        }
-
-        if (apiFound) {
             return true;
-        } else {
+        } catch (ClassNotFoundException e) {
             return false;
         }
     }
